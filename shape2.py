fd6ed6afff51a27078c5f37c0564dd93f1f1f294
ALex03-HELP-ME <alexxxlin168@gmail.com>
2025-02-06 09:24:29 -0500

    money

--- shape2.py
+++ shape2.py
@@ -3,6 +3,21 @@
 
 
 
+""" print("test")
+
+#def defines a function
+#inside the parenthesis are the inputs aka argument
+
+def add(x,y):
+    #print(x + y)
+    #return creates an output for the function
+    print(x + y)
+    return(x + Y)
+    add(5,6) """
+
+
+
+
 
 
 
